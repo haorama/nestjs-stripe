8f915e1914ea0b4757a844f293290a87ae1c1438
mlazuardy <lazuardymichael@gmail.com>
2021-08-25 14:08:19 +0700

    remove webhook config

--- lib/interfaces/stripe-module.options.ts
+++ lib/interfaces/stripe-module.options.ts
@@ -1,9 +1,7 @@
 import Stripe from 'stripe';
-import { StripeWebhookOptions } from './stripe-webhook.options';
 
 export interface StripeModuleOptions {
   global?: boolean
-  webhook?: StripeWebhookOptions,
   stripeConfig?: Stripe.StripeConfig
   secretKey: string
 }
