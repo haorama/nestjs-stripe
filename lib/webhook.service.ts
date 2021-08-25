bca84f52e3451de0cce004cc8787677a38dc6a33
mlazuardy <lazuardymichael@gmail.com>
2021-08-25 13:09:35 +0700

    lib change

--- lib/webhook.service.ts
+++ lib/webhook.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, OnApplicationBootstrap } from "@nestjs/common";
+import { Injectable, OnApplicationBootstrap } from "@nestjs/common";
 import { DiscoveryService, MetadataScanner, Reflector } from "@nestjs/core";
 import { InstanceWrapper } from "@nestjs/core/injector/instance-wrapper";
 import Stripe from "stripe";
@@ -7,40 +7,50 @@ import { STRIPE_EVENT_KEY } from "./constants";
 @Injectable()
 export class WebhookService implements OnApplicationBootstrap {
   constructor(
-    // private reflector: Reflector,
+    private reflector: Reflector,
     private readonly discoveryService: DiscoveryService,
     private readonly metadataScanner: MetadataScanner
   ) {}
 
+  protected webhookHandlers: any[] = []
 
   onApplicationBootstrap() {
-
+    this.loadWebhookHandler()
   }
 
-  handleWebhook(event: Stripe.Event) {
+  async handleWebhook(event: Stripe.Event) {
+    const handler = this.webhookHandlers.find(handler => handler.eventType == event.type);
 
+    if (handler) {
+      return handler.method(event)
+    }
   }
 
-  loadWebhookHandler() {
-    // const providers = this.discoveryService.getProviders();
-    //   const controllers = this.discoveryService.getControllers();
-    //   [...providers, ...controllers]
-    //     .filter(wrapper => wrapper.isDependencyTreeStatic())
-    //     .filter(wrapper => wrapper.instance)
-    //     .forEach((wrapper: InstanceWrapper) => {
-    //       const { instance } = wrapper;
+  private loadWebhookHandler() {
+    const providers = this.discoveryService.getProviders();
+      return providers
+        .filter(wrapper => wrapper.isDependencyTreeStatic())
+        .filter(wrapper => wrapper.instance)
+        .forEach((wrapper: InstanceWrapper) => {
+          const { instance } = wrapper;
 
-    //       const prototype = Object.getPrototypeOf(instance);
-    //       this.metadataScanner.scanFromPrototype(
-    //         instance,
-    //         prototype,
-    //         (methodKey: string) =>
-    //           this.subscribeToWebhookEvent(instance, methodKey),
-    //       );
-    //   });
+          const prototype = Object.getPrototypeOf(instance);
+          this.metadataScanner.scanFromPrototype(
+            instance,
+            prototype,
+            (methodKey: string) =>
+              this.setWebhookHandler(instance, methodKey),
+          );
+      });
   }
 
-  subscribeToWebhookEvent(instance: any, methodKey: any) {
+  private setWebhookHandler(instance: any, methodKey: any) {
+    const eventType = this.reflector.get(STRIPE_EVENT_KEY, instance[methodKey])
 
+    if (!eventType) return;
+
+    this.webhookHandlers.push({
+      eventType, method: instance[methodKey]
+    })
   }
 }
