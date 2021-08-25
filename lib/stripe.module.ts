09323a241dae4d25f309c45a955e5cb0c3b5a6e8
mlazuardy <lazuardymichael@gmail.com>
2021-08-25 12:40:57 +0700

    add

--- lib/stripe.module.ts
+++ lib/stripe.module.ts
@@ -1,22 +1,25 @@
-import { DynamicModule, Module, Provider } from "@nestjs/common";
+import { DynamicModule, Global, Module } from "@nestjs/common";
 import { STRIPE_SERVICE } from "./constants";
 import { StripeModuleOptions } from "./interfaces";
 import Stripe from 'stripe';
 import { WebhookService } from "./webhook.service";
+import { DiscoveryModule } from "@nestjs/core";
 
+@Global()
 @Module({})
 export class StripeModule {
   static register(options: StripeModuleOptions): DynamicModule {
-    const provider: Provider = {
+    const stripeProvider = {
       provide: STRIPE_SERVICE,
-      useValue: new Stripe(options.secretKey, options.stripeConfig)
+      useValue: new Stripe(options.secretKey, options.stripeConfig),
     }
 
     return {
-      providers: [provider, WebhookService],
-      global: options.global ?? true,
       module: StripeModule,
-      exports: [provider],
+      global: options.global ?? true,
+      providers: [WebhookService, stripeProvider],
+      exports: [WebhookService, stripeProvider],
+      imports: [DiscoveryModule]
     }
   }
 }
--- lib/webhook.service.ts
+++ lib/webhook.service.ts
@@ -1,8 +1,46 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable, OnApplicationBootstrap } from "@nestjs/common";
+import { DiscoveryService, MetadataScanner, Reflector } from "@nestjs/core";
+import { InstanceWrapper } from "@nestjs/core/injector/instance-wrapper";
+import Stripe from "stripe";
+import { STRIPE_EVENT_KEY } from "./constants";
 
 @Injectable()
-export class WebhookService {
-  handleWebhook(type: string, payload: any) {
+export class WebhookService implements OnApplicationBootstrap {
+  constructor(
+    // private reflector: Reflector,
+    private readonly discoveryService: DiscoveryService,
+    private readonly metadataScanner: MetadataScanner
+  ) {}
+
+
+  onApplicationBootstrap() {
+
+  }
+
+  handleWebhook(event: Stripe.Event) {
+
+  }
+
+  loadWebhookHandler() {
+    // const providers = this.discoveryService.getProviders();
+    //   const controllers = this.discoveryService.getControllers();
+    //   [...providers, ...controllers]
+    //     .filter(wrapper => wrapper.isDependencyTreeStatic())
+    //     .filter(wrapper => wrapper.instance)
+    //     .forEach((wrapper: InstanceWrapper) => {
+    //       const { instance } = wrapper;
+
+    //       const prototype = Object.getPrototypeOf(instance);
+    //       this.metadataScanner.scanFromPrototype(
+    //         instance,
+    //         prototype,
+    //         (methodKey: string) =>
+    //           this.subscribeToWebhookEvent(instance, methodKey),
+    //       );
+    //   });
+  }
+
+  subscribeToWebhookEvent(instance: any, methodKey: any) {
 
   }
 }
